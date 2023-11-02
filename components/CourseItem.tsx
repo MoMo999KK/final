3036307dc81c595c62d15693b5db44bad843803a
MoMo999KK <zamanij34@gmail.com>
2023-11-02 16:34:29 +0100

    added iniData

--- components/CourseItem.tsx
+++ components/CourseItem.tsx
@@ -3,10 +3,10 @@ import { UserCourse } from "@prisma/client"
 interface Props{
     initialDta:UserCourse[] | null
 }
-export const CourseItem = ({initialData}:Props) => {
+export const CourseItem = ({initialDta}:Props) => {
   return (
     <div>
-        {initialData.map((course:UserCourse)=>(
+        {initialDta?.map((course:UserCourse)=>(
             <div className="" key={course.id}>
 
                 {course.name}
